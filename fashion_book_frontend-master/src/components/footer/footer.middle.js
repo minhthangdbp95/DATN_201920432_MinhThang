dac54f967df7668fc7b3115d027e36da5e055de3
minhthangdbp95 <minhthangdbp95@gmail.com>
2022-03-14 21:05:19 +0700

    first commit

---
+++ fashion_book_frontend-master/src/components/footer/footer.middle.js
@@ -0,0 +1,42 @@
+	import React, { Component } from "react"
+
+	const FooterMiddle = () => (
+		<div className="footer-widget">
+				<div className="container">
+					<div className="row">
+					<div className="col-sm-3">
+							<div className="single-widget">
+								<h2>Tlike Shop</h2>
+								<ul className="nav nav-pills nav-stacked ">
+								<p>Tlike Shop là một website chuyên bán Quần áo, phụ kiện, bao gồm nhiều thể loại đa dạng và phong phú 
+                            , chúng tôi muốn cung cấp các trải nhiệm tuyệt vời nhất đên khách hàng
+                        </p>
+								</ul>
+							</div>
+						</div>
+						<div className="col-sm-3">
+							<div className="single-widget">
+								<h2></h2>
+								<ul className="nav nav-pills nav-stacked ">
+								</ul>
+							</div>
+						</div>
+						<div className="col-sm-3">
+							<div className="single-widget">
+								<h2>Thông Tin Về Chúng Tôi</h2>
+								<ul className="nav nav-pills nav-stacked ">
+									<li><i className='fas fa-map-marker-alt'></i><a href="#">Đặng Minh Thắng</a></li>
+									<li><i className='far fa-envelope'></i><a href="#">dangminhthang@gmail.com</a></li>
+									<li><i className='fas fa-phone-volume'></i><a href="#">01234567899</a></li>
+									<li><i class="fab fa-facebook-f"></i><a href="#">dangminhthang</a></li>
+									
+								</ul>
+							</div>
+						</div>
+						
+						
+					</div>
+				</div>
+			</div>
+	)
+	export default FooterMiddle
---
+++ fashion_book_frontend-master/src/components/footer/footer.top.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+const FooterTop = () => (
+    <div className="footer-top">
+        
+    </div>
+)
+export default FooterTop
